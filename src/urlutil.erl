1ae0147d2d1e4517e45c3ce25f33fa147912b0d7
Seth Falcon <seth@userprimary.net>
2009-02-08 10:22:24 -0800

    Basic parse_url working

--- src/urlutil.erl
+++ src/urlutil.erl
@@ -8,12 +8,18 @@
 %% @doc Parses a URL into scheme, host, port, path, and query parameters.
 %% all values are strings except port (integer) and query parameters, a
 %% list of tuples.
-
-port_from_acc(PortAcc) ->
-    if
-        length(PortAcc) > 0 ->
-            list_to_integer(lists:reverse(PortAcc));
-        true -> 0
+parse_url(Url) ->
+    case parse_scheme(Url) of
+        {ok, Scheme, Rest} ->
+            case parse_host(Rest) of
+                {error, Why, _} ->
+                    {error, Why, Url};
+                {Host, Port, PathWithParams} ->
+                    {Path, Params} = parse_params(PathWithParams),
+                    {Scheme, Host, Port, Path, Params}
+            end;
+        {error, Why, _} ->
+            {error, Why, Url}
     end.
 
 parse_host(Url) ->
@@ -26,13 +32,13 @@ parse_host([$:|[C|Rest]], [], Tsoh)
   when is_integer(C), C > $0, C < $9 ->
     parse_host(Rest, [C], Tsoh);
 parse_host([$:|[C|Rest]], [], Tsoh) ->
-    {lists:reverse(Tsoh), error, [C|Rest]};
+    {error, bad_port, lists:reverse(Tsoh) ++ ":" ++ [C|Rest]};
 parse_host([C|Rest], PortAcc, Tsoh)
   when length(PortAcc) > 0, is_integer(C), C > $0, C < $9 ->
     parse_host(Rest, [C|PortAcc], Tsoh);
 parse_host([C|Rest], PortAcc, Tsoh)
   when length(PortAcc) > 0, is_integer(C) ->
-    {lists:reverse(Tsoh), error, [C|Rest]};
+    {error, bad_port, lists:reverse(Tsoh) ++ ":" ++ [C|Rest]};
 parse_host([C|Rest], PortAcc, Acc) when length(PortAcc) =:= 0 ->
     parse_host(Rest, PortAcc, [C|Acc]).
 
@@ -42,6 +48,22 @@ parse_scheme(Url) ->
             {ok, string:substr(Url, 1, Pos - 1), string:substr(Url, Pos + 3)};  
        true ->
             {error, no_scheme, Url}
+    end.
+
+parse_params(Url) ->
+    Pos = string:chr(Url, $?),
+    if Pos > 0 ->
+            Params = lists:map(fun([K|[$=|V]]) -> {[K], V} end,
+                               string:tokens(string:substr(Url, Pos + 1), "&")),
+            {string:substr(Url, 1, Pos - 1), Params};
+       true -> {Url, []}
+    end.
+
+port_from_acc(PortAcc) ->
+    if
+        length(PortAcc) > 0 ->
+            list_to_integer(lists:reverse(PortAcc));
+        true -> 0
     end.
 
 parse_scheme_test_() ->
@@ -58,7 +80,7 @@ parse_host_test_() ->
      ?_assertMatch({"foo.bar.com",123,[]}, parse_host("foo.bar.com:123")),
      ?_assertMatch({"foo.bar.com",0,"/a/b"}, parse_host("foo.bar.com/a/b")),
      ?_assertMatch({"f.com",4,"/a/b"}, parse_host("f.com:4/a/b")),
-     ?_assertMatch({"f.com",error,"x"}, parse_host("f.com:x")),
-     ?_assertMatch({"f.com",error,"y2/"}, parse_host("f.com:1y2/"))
+     ?_assertMatch({error,bad_port,"f.com:x"}, parse_host("f.com:x")),
+     ?_assertMatch({error,bad_port,"f.com:y2/"}, parse_host("f.com:1y2/"))
     ].
 
